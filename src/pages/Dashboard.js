53968e2a22efb03a756f79ee097673e81a5aee7e
MaheshNagaladinne <nagaladinnemahesh07@gmail.com>
2025-09-22 22:33:30 +0530

    setup folder structure, installed dependencies, dashboard

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+function Dashboard(){
+    return (
+        <div>
+            <h1>User Management Dashboard</h1>
+            <p>Manage Users - view, add, edit and delete</p>
+        </div>
+    )
+}
+
+export default Dashboard;
